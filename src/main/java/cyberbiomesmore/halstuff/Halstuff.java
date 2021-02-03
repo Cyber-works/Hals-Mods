58427929caf8adc5c806d49562511113f0d9572c
Cyber-works <Cyber-works@users.noreply.github.com>
2021-02-03 17:35:32 -0500

    First Addition of Pizza Item

--- src/main/java/cyberbiomesmore/halstuff/Halstuff.java
+++ src/main/java/cyberbiomesmore/halstuff/Halstuff.java
@@ -1,10 +1,36 @@
 package cyberbiomesmore.halstuff;
 
+import jdk.nashorn.internal.ir.Block;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.CakeBlock;
+import net.minecraft.block.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Halstuff implements ModInitializer {
+
+    // ITEM Initialization
+    public static final Item CHEESE_PIZZA = new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
+
+    // BLOCK Initialization
+
+    public static final Block PIZZA_CHEESE = new CakeBlock();
+
+    //public static final net.minecraft.block.Block CAKE = register("cake", new CakeBlock(net.minecraft.block.Block.Properties.create(Material.CAKE).hardnessAndResistance(0.5F).sound(SoundType.CLOTH)));
+
     @Override
     public void onInitialize() {
+
+
+        // Registering new ITEMS
+        Registry.register(Registry.ITEM, new Identifier("halstuff", "cheese_pizza"), CHEESE_PIZZA);
+
+        // Registering new BLOCKS
+        Registry.register(Registry.BLOCK, new Identifier("halstuff", "pizza_cheese"), PIZZA_CHEESE);
 
     }
 }
---
+++ src/main/java/cyberbiomesmore/halstuff/blocks/PizzaBlock.java
@@ -0,0 +1,10 @@
+package cyberbiomesmore.halstuff.blocks;
+
+import net.minecraft.block.Block;
+
+public class PizzaBlock extends Block {
+    
+    public PizzaBlock(Settings settings) {
+        super(settings);
+    }
+}
