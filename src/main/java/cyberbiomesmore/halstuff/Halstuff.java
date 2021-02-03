b7344e4f28f8b25c202b271e2472180a7d633dc0
Cyber-works <Cyber-works@users.noreply.github.com>
2021-02-03 15:11:10 -0500

    First Commit

---
+++ src/main/java/cyberbiomesmore/halstuff/Halstuff.java
@@ -0,0 +1,10 @@
+package cyberbiomesmore.halstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Halstuff implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
