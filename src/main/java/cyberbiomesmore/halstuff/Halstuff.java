72b105eaa55152ccb8f23191330630f5f3fcaba4
Cyber-works <Cyber-works@users.noreply.github.com>
2021-02-03 19:32:18 -0500

    Lots of changes

--- src/main/java/cyberbiomesmore/halstuff/Halstuff.java
+++ src/main/java/cyberbiomesmore/halstuff/Halstuff.java
@@ -1,11 +1,13 @@
 package cyberbiomesmore.halstuff;
 
+import cyberbiomesmore.halstuff.blocks.PizzaBlock;
 import jdk.nashorn.internal.ir.Block;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.CakeBlock;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -18,7 +20,7 @@ public class Halstuff implements ModInitializer {
 
     // BLOCK Initialization
 
-    public static final Block PIZZA_CHEESE = new CakeBlock();
+    public static final PizzaBlock PIZZA_CHEESE = new PizzaBlock();
 
     //public static final net.minecraft.block.Block CAKE = register("cake", new CakeBlock(net.minecraft.block.Block.Properties.create(Material.CAKE).hardnessAndResistance(0.5F).sound(SoundType.CLOTH)));
 
@@ -26,11 +28,15 @@ public class Halstuff implements ModInitializer {
     public void onInitialize() {
 
 
-        // Registering new ITEMS
-        Registry.register(Registry.ITEM, new Identifier("halstuff", "cheese_pizza"), CHEESE_PIZZA);
 
         // Registering new BLOCKS
         Registry.register(Registry.BLOCK, new Identifier("halstuff", "pizza_cheese"), PIZZA_CHEESE);
+
+        // Registering new ITEMS
+        Registry.register(Registry.ITEM, new Identifier("halstuff", "cheese_pizza"),
+                new BlockItem(PIZZA_CHEESE, new FabricItemSettings().group(ItemGroup.MISC)));
+
+
 
     }
 }
--- src/main/java/cyberbiomesmore/halstuff/blocks/PizzaBlock.java
+++ src/main/java/cyberbiomesmore/halstuff/blocks/PizzaBlock.java
@@ -1,10 +1,31 @@
 package cyberbiomesmore.halstuff.blocks;
 
-import net.minecraft.block.Block;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
-public class PizzaBlock extends Block {
-    
-    public PizzaBlock(Settings settings) {
-        super(settings);
+public class PizzaBlock extends CakeBlock {
+
+    protected static final VoxelShape[] SHAPES = new VoxelShape[]{
+            Block.createCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D),
+            Block.createCuboidShape(3.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D),
+            Block.createCuboidShape(5.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D),
+            Block.createCuboidShape(7.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D),
+            Block.createCuboidShape(9.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D),
+            Block.createCuboidShape(11.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D),
+            Block.createCuboidShape(13.0D, 0.0D, 1.0D, 15.0D, 4.0D, 15.0D)
+    };
+
+    public PizzaBlock() {
+        super(FabricBlockSettings.of(Material.CAKE).breakByHand(true).hardness(0.5f).sounds(BlockSoundGroup.SHROOMLIGHT));
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return SHAPES[state.get(BITES)];
     }
 }
